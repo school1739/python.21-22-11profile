928adb38f23403c5433e38d4805e10fe089c0308
Saykiri <silver2004list.ru>
2022-04-19 07:01:55 +0000

    Поправленная версия

--- 24/1.py
+++ 24/1.py
@@ -1,13 +1,12 @@
-'''with open('24_demo.txt') as f:
-    s = f.readline()'''
-
-s = 'AXXBXXXCXXXXG'
-k = 1
-kmax = 1
-for i in range(1,len(s)):
-    if s[i] == s[i-1] == 'X':
-        k += 1
-        kmax = max(kmax,k)
-    else:
-        k=1
-print(kmax)
+with open('24_demo.txt') as f:
+    s = f.readline()
+    s = 'AXXBXXXCXXXXG'
+    k = 1
+    kmax = 1
+    for i in range(1,len(s)):
+        if s[i] == s[i-1] == 'X':
+            k += 1
+            kmax = max(kmax,k)
+        else:
+            k=1
+    print(kmax)
